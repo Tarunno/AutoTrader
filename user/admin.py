6f7bde95662df5cb7b26655b1809049b7a687f5e
Mahadi Hasan <mahadihasantarunno@gmail.com>
2023-06-09 17:02:32 +0600

    Admin

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Customer
 
-admin.site.site_header = 'Cars & Bids | admin'
+admin.site.site_header = 'AutoTrader | admin'
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
